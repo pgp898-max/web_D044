ddad4ea7ebb5329c83cf79cf97bd51cf24029a9f
pgp898-max <pgp898@gmail.com>
2026-03-29 02:02:34 +0530

    for Experiment 5

---
+++ src/MyFile44.js
@@ -0,0 +1,12 @@
+import React from 'react'; // 1. Import the React tool
+
+function MyFile() {       // 2. Create the function (The Component)
+  return (
+    <div>
+      <h1>Hello GitHub!</h1>
+      <p>This is my first React file.</p>
+    </div>
+  );
+}
+
+export default MyFile;    // 3. Export it so other files can use it
---
+++ src/login_page44.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <input type="text" placeholder="Username" />
+      <button>Login</button>
+    </div>
+  );
+};
+
+export default LoginPage;
